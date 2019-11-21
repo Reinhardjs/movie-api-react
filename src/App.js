82f150d7152852e1e1769a8e0cc625530cf47248
Reinhard Jonathan Silalahi <reinhardjsilalahi@gmail.com>
2019-11-21 22:59:56 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-layout-components';
+import {
+  Row, Col, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://sheetdb.io/api/v1/t8zwuswe7w45t')
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data)
+        // alert(JSON.stringify(data))
+      });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Container style={{ width: "1000px" }}>
+        <Row>
+          {
+            movies.map(movie => (
+
+              <Col md="4">
+                <Card>
+                  <CardImg top height="300" src={movie.imageUrl} alt="" />
+                  <CardBody>
+                    <CardTitle>{movie.title}</CardTitle>
+                    <CardText>{movie.description}</CardText>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+
+            ))
+          };
+        </Row>
+      </Container>
+
+    </>
+
   );
+
 }
 
 export default App;
